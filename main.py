8f3f05542369a36c782ab7f1eaa422943fce1564
Anthony J. Proctor <anthonyproctor@gmail.com>
2022-08-11 21:39:14 -0600

    Update to main

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #Welcome to the Band Name Generator
-#By: Anthony Proctor
-print("Hello, and welcome to my Band Name Generator!")
+#By: Anthony ProctorR
+print("Hello, and welcome to my Band Name Generator!\n")
 
-city = input("What is the name of the city that you grew up in?")
+city = input("What is the name of the city that you grew up in?\n")
 
-pet = input("What is the name of a pet?")
+pet = input("What is the name of a pet?\n")
 
-bandName = city + "" + pet
+bandName = city + " " + pet
 print("Your band name could be " + bandName)
